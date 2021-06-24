14efa561207e995673e6c64afe40c775eebc0531
Generally2 <dailyspecial_719@yahoo.com>
2021-06-23 22:28:12 -0600

    created TicTacToe controller in the MainActivity still debugging

--- app/src/main/java/com/generally2/tictactoemvc/MainActivity.java
+++ app/src/main/java/com/generally2/tictactoemvc/MainActivity.java
@@ -3,12 +3,61 @@ package com.generally2.tictactoemvc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TicTacToeGame mGame = new TicTacToeGame();
+    private TextView mGameSateTextView;
+    private Button[][] mTicTacToeButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGame.resetGame();
+
+        mGameSateTextView = findViewById(R.id.message_txt);
+
+
+        Button newGameButton = findViewById(R.id.new_game_btn);
+        newGameButton.setOnClickListener(this);
+
+        mTicTacToeButtons = new Button[TicTacToeGame.NUM_ROWS][TicTacToeGame.NUM_COLUMNS];
+      //  mTicTacToeButtons[0][0]= findViewById(R.id.btn00);
+        for (int row = 0; row < TicTacToeGame.NUM_ROWS; row++){
+            for (int col = 0; col < TicTacToeGame.NUM_COLUMNS; col++){
+                int id = getResources().getIdentifier("button" + row + col, "id", getPackageName());
+                mTicTacToeButtons[row][col] = findViewById(id);
+                mTicTacToeButtons[row][col].setOnClickListener(this);
+            }
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.new_game_btn){
+            mGame.resetGame();
+        }
+
+        for (int row = 0; row < TicTacToeGame.NUM_ROWS; row++) {
+            for (int col = 0; col < TicTacToeGame.NUM_COLUMNS; col++) {
+                if (view.getId() == mTicTacToeButtons[row][col].getId()){
+                    Log.d("TTT", "Button pressed at " + row + " " + col);
+
+                }
+                mGameSateTextView.setText(mGame.stringForGameState());
+
+
+            }
+        }
+
+
+
+        //you are here, game not working 13:57 on controller video
     }
 }
--- app/src/main/java/com/generally2/tictactoemvc/TicTacToeGame.java
+++ app/src/main/java/com/generally2/tictactoemvc/TicTacToeGame.java
@@ -12,131 +12,133 @@ public class TicTacToeGame {
         X_WIN,
         O_WIN,
         TIE_GAME;
+    }
 
-        private GameState gameState;
-        private int [][] boardArray;
-        private  Context context;
+    private GameState gameState;
+    private int[][] boardArray;
+    private Context context;
 
-        public static final int NUM_ROWS = 3;
-        public static final int NUM_COLUMNS = 3;
+    public static final int NUM_ROWS = 3;
+    public static final int NUM_COLUMNS = 3;
 
-        private static final int MARK_NONE = 0;
-        private static final int MARK_X = 1;
-        private static final int MARK_O = 2;
+    private static final int MARK_NONE = 0;
+    private static final int MARK_X = 1;
+    private static final int MARK_O = 2;
 
-        public void TicTacToeGame(Context context){
-            this.context = context;
-            resetGame();
+    public void TicTacToeGame(Context context) {
+        this.context = context;
+        resetGame();
+    }
+
+    public void resetGame() {
+        this.boardArray = new int[NUM_ROWS][NUM_COLUMNS];
+        this.gameState = GameState.X_TURN;
+    }
+
+    public void pressedButtonAtLocation(int row, int column) {
+        if (row < 0 || row > NUM_ROWS || column < 0 || column > NUM_COLUMNS) {
+            return; // not a valid square location
         }
-
-        private void resetGame() {
-            this.boardArray= new int[NUM_ROWS][NUM_COLUMNS];
+        if (this.boardArray[row][column] != MARK_NONE) {
+            return; //empty
+        }
+        if (this.gameState == GameState.X_TURN) {
+            this.boardArray[row][column] = MARK_X;
+            this.gameState = GameState.O_TURN;
+        } else if (this.gameState == GameState.O_TURN) {
+            this.boardArray[row][column] = MARK_O;
             this.gameState = GameState.X_TURN;
         }
 
-        public void pressedButtonAtLocation(int row,int column){
-            if (row < 0 || row > NUM_ROWS || column < 0 || column > NUM_COLUMNS){
-                return; // not a valid square location
-            }
-            if (this.boardArray[row][column] != MARK_NONE){
-                return; //empty
-            }
-            if (this.gameState == GameState.X_TURN){
-                this.boardArray[row][column] = MARK_X;
-                this.gameState = GameState.O_TURN;
-            } else  if (this.gameState ==GameState.O_TURN){
-                this.boardArray[row][column] = MARK_O;
-                this.gameState = GameState.X_TURN;
-            }
-
-            checkForWin();
-
-        }
-
-        private void checkForWin() {
-            if (!(this.gameState == GameState.X_TURN || this.gameState == GameState.O_TURN)) {
-                return;
-            }
-        }
-
-            private boolean isBoardFull(){
-                for (int row = 0; row < NUM_ROWS; row++){
-                    for (int col = 0; col<NUM_COLUMNS; col++){
-                        if (this.boardArray[row][col] == MARK_NONE){
-                            Log.d("TicTacToeGame", "Empty at Row: " + row + "Col: " + col);
-                            return false;
-
-                        }
-
-                    }
-
-                }
-                return true;
-            }
-
-        private boolean didPieceWin(int markType){
-            boolean allMarksMatch = true;
-            for (int col = 0; col < NUM_COLUMNS;col++){
-                allMarksMatch = true;
-                for (int row = 0; row < NUM_ROWS; row++){
-                    if (this.boardArray[row][col] != markType){
-                        allMarksMatch = false;
-                        break;
-
-                    }
-                }
-                if (allMarksMatch){
-                    return true;
-                }
-
-            }
-
-            for (int row = 0; row < NUM_ROWS; row++){
-                allMarksMatch = true;
-                for (int col = 0; col < NUM_COLUMNS; col++){
-                    if (this.boardArray[row][col] != markType){
-                        allMarksMatch = false;
-                        break;
-                    }
-                }
-                if (allMarksMatch){
-                    return true;
-
-                }
-            }
-            if (this.boardArray[0][0] == markType && this.boardArray[1][1] == markType && this.boardArray[2][2] == markType){
-                return true;
-            }
-
-            if (this.boardArray[2][0] ==markType && this. boardArray[1][1] == markType && this.boardArray[0][2] == markType){
-                return true;
-
-            }
-
-            return false;
-        }
-        public String stringForGameState(){
-            String gameStateLabel = "";
-            Resources r = this.context.getResources();
-            switch (this.gameState){
-                case X_TURN:
-                    gameStateLabel = r.getString(R.string.x_turn);
-                    break;
-                case O_TURN:
-                    gameStateLabel = r.getString(R.string.o_turn);
-                    break;
-                case X_WIN:
-                    gameStateLabel = r.getString(R.string.x_win);
-                    break;
-                case O_WIN:
-                    gameStateLabel = r.getString(R.string.o_win);
-                    break;
-                default:
-                    gameStateLabel = r.getString(R.string.tie_game);
-                    break;
-            }
-            return gameStateLabel;
-        }
+        checkForWin();
 
     }
+
+    private void checkForWin() {
+        if (!(this.gameState == GameState.X_TURN || this.gameState == GameState.O_TURN)) {
+            return;
+        }
+    }
+
+    private boolean isBoardFull() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLUMNS; col++) {
+                if (this.boardArray[row][col] == MARK_NONE) {
+                    Log.d("TicTacToeGame", "Empty at Row: " + row + "Col: " + col);
+                    return false;
+
+                }
+
+            }
+
+        }
+        return true;
+    }
+
+    private boolean didPieceWin(int markType) {
+        boolean allMarksMatch = true;
+        for (int col = 0; col < NUM_COLUMNS; col++) {
+            allMarksMatch = true;
+            for (int row = 0; row < NUM_ROWS; row++) {
+                if (this.boardArray[row][col] != markType) {
+                    allMarksMatch = false;
+                    break;
+
+                }
+            }
+            if (allMarksMatch) {
+                return true;
+            }
+
+        }
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            allMarksMatch = true;
+            for (int col = 0; col < NUM_COLUMNS; col++) {
+                if (this.boardArray[row][col] != markType) {
+                    allMarksMatch = false;
+                    break;
+                }
+            }
+            if (allMarksMatch) {
+                return true;
+
+            }
+        }
+        if (this.boardArray[0][0] == markType && this.boardArray[1][1] == markType && this.boardArray[2][2] == markType) {
+            return true;
+        }
+
+        if (this.boardArray[2][0] == markType && this.boardArray[1][1] == markType && this.boardArray[0][2] == markType) {
+            return true;
+
+        }
+
+        return false;
+    }
+
+    public String stringForGameState() {
+        String gameStateLabel = "";
+        Resources r = this.context.getResources();
+        switch (this.gameState) {
+            case X_TURN:
+                gameStateLabel = r.getString(R.string.x_turn);
+                break;
+            case O_TURN:
+                gameStateLabel = r.getString(R.string.o_turn);
+                break;
+            case X_WIN:
+                gameStateLabel = r.getString(R.string.x_win);
+                break;
+            case O_WIN:
+                gameStateLabel = r.getString(R.string.o_win);
+                break;
+            default:
+                gameStateLabel = r.getString(R.string.tie_game);
+                break;
+        }
+        return gameStateLabel;
+    }
+
+
 }
